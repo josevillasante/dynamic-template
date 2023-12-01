de3b776c6b3d11e0f72c609b708748f4ca739e38
Jose Villasante <jose.villasante@visma.com>
2023-12-01 15:24:53 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './component/list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dynamic-template';
+  name = 'Angular ' + VERSION.major;
 }
